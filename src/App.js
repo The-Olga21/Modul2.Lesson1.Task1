6f8fa4f01a7ff4213bba2378c23ef79648e51416
Olga Prokofeva <o1ga-prokofeva@mail.ru>
2024-05-28 01:16:50 +0300

    Create without JSX

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
 	const currentYear = new Date().getFullYear();
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="Current-year">
-					<p>Текущий год: {currentYear}</p>
-				</div>
-			</header>
-		</div>
+	const app = createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement('p', null, 'Edit src/App.js and save to reload.'),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement(
+				'div',
+				{ className: 'Current-year' },
+				createElement('p', null, `Текущий год: ${currentYear}`),
+			),
+		),
 	);
+	//,
+
+	return app;
 };
